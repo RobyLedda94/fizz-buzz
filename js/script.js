76cc55086854bbbba682d304989b9f41cea0e1d2
RobyLedda94 <robyledda01@yahoo.it>
2025-09-01 14:21:23 +0200

    [UPD] dichiarazione variabili elementi di visualizazzione e interazione

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('prova');
+// Dichiarazione delle variabili
+
+let btn_fizzBuzz = document.getElementById('btn-fizzBuzz'); // Bottone
+
+
+let grid_container = document.getElementById('grid-container') // Contenitore della griglia
+
+
+let gridfizzBuzz = document.getElementById('gridFizzBuzz') // Griglia FizzBuzz
+
+
+// Debugging
+
+console.log(btn_fizzBuzz, grid_container, gridfizzBuzz);
